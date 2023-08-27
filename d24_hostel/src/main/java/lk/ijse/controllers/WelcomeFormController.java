9af39b390b3a480f9eb49050176024bcb260d33d
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-27 23:27:53 +0530

    loading bar and ui is done

--- d24_hostel/src/main/java/lk/ijse/controllers/WelcomeFormController.java
+++ d24_hostel/src/main/java/lk/ijse/controllers/WelcomeFormController.java
@@ -1,4 +1,59 @@
 package lk.ijse.controllers;
 
-public class WelcomeFormController {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WelcomeFormController implements Initializable {
+    @FXML
+    private Label loadingLbl;
+
+    @FXML
+    private Rectangle fullLoadingBar;
+    @FXML
+    private Rectangle loadingBar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        init();
+    }
+
+    private void init() {
+        Timeline timeline = new Timeline();
+
+        KeyFrame keyFrame1= new KeyFrame(Duration.millis(2500), actionEvent ->{
+            loadingBar.setWidth(fullLoadingBar.getWidth()*0.2);
+        });
+
+        KeyFrame keyFrame2= new KeyFrame(Duration.millis(3000),actionEvent ->{
+            loadingLbl.setText("Loading Internal Resources....");
+            loadingBar.setWidth(fullLoadingBar.getWidth()*0.5);
+        });
+        KeyFrame keyFrame3= new KeyFrame(Duration.millis(3500),actionEvent ->{
+            loadingLbl.setText("Loading Images....");
+            loadingBar.setWidth(fullLoadingBar.getWidth()*0.6);
+        });
+        KeyFrame keyFrame4= new KeyFrame(Duration.millis(4000),actionEvent ->{
+            loadingLbl.setText("Loading UIs....");
+            loadingBar.setWidth(fullLoadingBar.getWidth()*0.8);
+        });
+        KeyFrame keyFrame5= new KeyFrame(Duration.millis(4500),actionEvent ->{
+            loadingLbl.setText("Welcome to D-24 v1.0.0");
+            loadingBar.setWidth(fullLoadingBar.getWidth());
+        });
+        KeyFrame keyFrame6= new KeyFrame(Duration.millis(5000),actionEvent ->{
+//            add navigation to login page
+        });
+
+        timeline.getKeyFrames().addAll(keyFrame1,keyFrame2,keyFrame3,keyFrame4,keyFrame5,keyFrame6);
+        timeline.playFromStart();
+    }
 }
