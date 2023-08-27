037e5485b2e89615165387053b58ef8635bc0d72
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-27 23:02:35 +0530

    load welcome page

--- d24_hostel/src/main/java/lk/ijse/Main.java
+++ d24_hostel/src/main/java/lk/ijse/Main.java
@@ -1,11 +1,20 @@
 package lk.ijse;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/views/welcomeForm.fxml")));
+        stage.setScene(scene);
+        stage.setTitle("D-24");
+        stage.getIcons().add(new Image("/assets/d23_logo.png"));
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
     }
 }
