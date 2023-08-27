097d8b10d234c8a9e81d9bcb5c1380246cd17078
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-27 22:58:18 +0530

    add welcome page

--- d24_hostel/src/main/java/lk/ijse/Main.java
+++ d24_hostel/src/main/java/lk/ijse/Main.java
@@ -1,7 +1,11 @@
 package lk.ijse;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
---
+++ d24_hostel/src/main/java/lk/ijse/controllers/WelcomeFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.controllers;
+
+public class WelcomeFormController {
+}
