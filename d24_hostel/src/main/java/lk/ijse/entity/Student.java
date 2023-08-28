1ee930ff6024c14c741eb75f38a5b5fb4fbbdf01
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-28 15:57:27 +0530

    student entity done

---
+++ d24_hostel/src/main/java/lk/ijse/entity/Student.java
@@ -0,0 +1,32 @@
+package lk.ijse.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "student_id")
+    private String id;
+    @Column(name = "student_name")
+    private String name;
+    @Column(name = "student_id")
+    private String address;
+    @Column(name = "student_contact")
+    private String contact;
+    @Column(name = "student_dob")
+    private Date dob;
+    @Column(name = "student_gender")
+    private String gender;
+}
