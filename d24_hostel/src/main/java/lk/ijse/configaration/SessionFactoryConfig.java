42d5e15a35c462bae30479fd65fcdc1e2cc10af8
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-28 15:48:14 +0530

    create session factory

---
+++ d24_hostel/src/main/java/lk/ijse/configaration/SessionFactoryConfig.java
@@ -0,0 +1,24 @@
+package lk.ijse.configaration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfig {
+    private static SessionFactoryConfig factoryConfig;
+    private final SessionFactory sessionFactory;
+
+    private SessionFactoryConfig() {
+        sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    public static SessionFactoryConfig getInstance() {
+        return (null == factoryConfig) ? factoryConfig = new SessionFactoryConfig() : factoryConfig;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
