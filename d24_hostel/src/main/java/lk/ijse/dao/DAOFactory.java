88a6531b124f5788da8d0ff3cdbf55be980bb8f9
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-31 02:38:06 +0530

    implement DAOFactory

--- d24_hostel/src/main/java/lk/ijse/dao/DAOFactory.java
+++ d24_hostel/src/main/java/lk/ijse/dao/DAOFactory.java
@@ -1,4 +1,26 @@
 package lk.ijse.dao;
 
+import lk.ijse.bo.custom.impl.UserBoImpl;
+
 public class DAOFactory {
+    private static DAOFactory factory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getInstance(){
+        return factory == null ? new DAOFactory() : factory;
+    }
+
+    public enum DAOTypes{
+        USER
+    }
+
+    public <T extends SuperDAO>T getDAO(DAOTypes types){
+        switch (types){
+            case USER:
+                return (T) new UserBoImpl();
+            default:
+                return null;
+        }
+    }
 }
