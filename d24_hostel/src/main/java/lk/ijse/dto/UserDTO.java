9a7181c921b936d38d70fb6b951c8380bbd41c9d
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-30 08:49:23 +0530

    add user dto

---
+++ d24_hostel/src/main/java/lk/ijse/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+    private String userName;
+    private String password;
+}
