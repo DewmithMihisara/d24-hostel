13dd2810b30df8d964c2a87ac0521cc7a466778e
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-30 08:26:52 +0530

    add skeleton into sign up page

--- d24_hostel/src/main/java/lk/ijse/controllers/SignUpFormController.java
+++ d24_hostel/src/main/java/lk/ijse/controllers/SignUpFormController.java
@@ -1,4 +1,78 @@
 package lk.ijse.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+
 public class SignUpFormController {
+    @FXML
+    private PasswordField pwTxt;
+
+    @FXML
+    private Button pwViewBtn;
+
+    @FXML
+    private PasswordField rePwTxt;
+
+    @FXML
+    private Button rePwViewBtn;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Button signInBtn;
+
+    @FXML
+    private TextField userNameTxt;
+
+    @FXML
+    private Line usrNameLine;
+
+    @FXML
+    private Line usrNameLine1;
+
+    @FXML
+    private Line usrNameLine2;
+
+    @FXML
+    private ImageView viewImg;
+
+    @FXML
+    private ImageView viewImg1;
+
+    @FXML
+    void pwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pwViewBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rePwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rePwViewBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userNameTxtOnAction(ActionEvent event) {
+
+    }
 }
