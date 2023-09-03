3bda0418300e0702d99b23d97af07ca3cd4ac32e
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-09-03 11:55:44 +0530

    add dashboard skeleton

--- d24_hostel/src/main/java/lk/ijse/controllers/DashBoardFormController.java
+++ d24_hostel/src/main/java/lk/ijse/controllers/DashBoardFormController.java
@@ -1,4 +1,45 @@
 package lk.ijse.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class DashBoardFormController {
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private AnchorPane bodyPane;
+
+    @FXML
+    private AnchorPane settingPane;
+
+    @FXML
+    void homeBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reservationBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roomsBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settingBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stdBtnOnAction(ActionEvent event) {
+
+    }
 }
