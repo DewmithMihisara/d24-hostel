93b38cd94ec78adfedea54abb037338ee65dedfe
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-09-07 19:51:53 +0530

    add query dao

--- d24_hostel/src/main/java/lk/ijse/dao/DAOFactory.java
+++ d24_hostel/src/main/java/lk/ijse/dao/DAOFactory.java
@@ -1,31 +1,34 @@
 package lk.ijse.dao;
 
 import lk.ijse.bo.custom.impl.UserBoImpl;
-import lk.ijse.dao.custom.impl.RoomDAOImpl;
-import lk.ijse.dao.custom.impl.StudentDAOImpl;
-import lk.ijse.dao.custom.impl.UserDAOImpl;
+import lk.ijse.dao.custom.impl.*;
 
 public class DAOFactory {
     private static DAOFactory factory;
 
-    private DAOFactory(){}
+    private DAOFactory() {
+    }
 
-    public static DAOFactory getInstance(){
+    public static DAOFactory getInstance() {
         return factory == null ? new DAOFactory() : factory;
     }
 
-    public enum DAOTypes{
-        USER, STUDENT, ROOM
+    public enum DAOTypes {
+        USER, STUDENT, ROOM, RESERVATION, QUERY
     }
 
-    public <T extends SuperDAO>T getDAO(DAOTypes types){
-        switch (types){
+    public <T extends SuperDAO> T getDAO(DAOTypes types) {
+        switch (types) {
             case USER:
                 return (T) new UserDAOImpl();
             case STUDENT:
                 return (T) new StudentDAOImpl();
             case ROOM:
                 return (T) new RoomDAOImpl();
+            case RESERVATION:
+                return (T) new ReservationDAOImpl();
+            case QUERY:
+                return (T) new QueryDAOImpl();
             default:
                 return null;
         }
