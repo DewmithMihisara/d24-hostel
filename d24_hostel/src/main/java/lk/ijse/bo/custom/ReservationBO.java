d8d5291cfdb60a3838ff92c7416e35b85a7dc58a
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-09-07 14:08:26 +0530

    res bo just create

--- d24_hostel/src/main/java/lk/ijse/bo/custom/ReservationBO.java
+++ d24_hostel/src/main/java/lk/ijse/bo/custom/ReservationBO.java
@@ -1,6 +1,11 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBo;
+import lk.ijse.dto.ReservationDTO;
+import lk.ijse.dto.StudentDTO;
+
+import java.util.List;
 
 public interface ReservationBO extends SuperBo {
+    List<ReservationDTO> getAllReservation();
 }
--- d24_hostel/src/main/java/lk/ijse/bo/custom/impl/ReservationBOImpl.java
+++ d24_hostel/src/main/java/lk/ijse/bo/custom/impl/ReservationBOImpl.java
@@ -1,6 +1,13 @@
 package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.custom.ReservationBO;
+import lk.ijse.dto.ReservationDTO;
+
+import java.util.List;
 
 public class ReservationBOImpl implements ReservationBO {
+    @Override
+    public List<ReservationDTO> getAllReservation() {
+        return null;
+    }
 }
